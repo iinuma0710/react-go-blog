e2651796d0cc3a79643d6f875fc644724807affc
Yuko Iinuma <iinuma0710@gmail.com>
2024-09-06 21:52:55 +0900

    20240906 server.go と mux.go に一部の機能を移植

--- backend/main.go
+++ backend/main.go
@@ -5,11 +5,9 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"net/http"
 	"os"
 
 	"github.com/iinuma0710/react-go-blog/backend/config"
-	"golang.org/x/sync/errgroup"
 )
 
 func main() {
@@ -30,38 +28,17 @@ func run(ctx context.Context) error {
 	// 環境変数 BACKEND_PORT で設定されたポートのリッスンを開始
 	l, err := net.Listen("tcp", fmt.Sprintf(":%d", cfg.BckendPort))
 	if err != nil {
-		log.Fatalf("failed to listen port %s: %v", cfg.BckendPort, err)
+		log.Fatalf("failed to listen port %d: %v", cfg.BckendPort, err)
 	}
 
 	// サーバの URL を表示
 	url := fmt.Sprintf("http://%s", l.Addr().String())
 	log.Printf("start with: %v", url)
 
-	s := &http.Server{
-		// Addr フィールドは指定しない
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintf(w, "Hello, %s!\n", r.URL.Path[1:])
-		}),
-	}
+	// ルーティングの設定を取得
+	mux := NewMux()
 
-	eg, ctx := errgroup.WithContext(ctx)
-
-	// eg.Go メソッドで HTTP サーバを起動するゴルーチンを立ち上げる
-	eg.Go(func() error {
-		// Server メソッドで HTTP サーバを立ち上げ
-		if err := s.Serve(l); err != nil && err != http.ErrServerClosed {
-			log.Printf("failed to close: %+v", err)
-			return err
-		}
-		return nil
-	})
-
-	// チャネルからの終了通知を待機
-	<-ctx.Done()
-	if err := s.Shutdown(context.Background()); err != nil {
-		log.Printf("failed to shutdown: %+v", err)
-	}
-
-	// Go メソッドで起動したゴルーチンの終了を待つ
-	return eg.Wait()
+	// Server 型のインスタンスを生成し、HTTP サーバを起動
+	s := NewServer(l, mux)
+	return s.Run(ctx)
 }
---
+++ backend/mux.go
@@ -0,0 +1,13 @@
+package main
+
+import "net/http"
+
+func NewMux() http.Handler {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		// 静的解析ツールのエラー回避のため、明示的に戻り値を捨てる
+		_, _ = w.Write([]byte(`{"status": "ok"}`))
+	})
+	return mux
+}
