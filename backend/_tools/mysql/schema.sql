442de91fa40e56084ff5c68f507988efcddc8ab8
Yuko Iinuma <iinuma0710@gmail.com>
2024-09-26 14:43:51 +0900

    20240926 RDBMS のテストを記述

--- backend/_tools/mysql/schema.sql
+++ backend/_tools/mysql/schema.sql
@@ -15,10 +15,10 @@ CREATE TABLE `article`
 (
     `id`         BIGINT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '記事の識別子',
     `title`      VARCHAR(128)    NOT NULL COMMENT '記事のタイトル',
-    `content`    VARCHAR(20)     NOT NULL COMMENT '記事の本文',
+    -- `content`    VARCHAR(20)     NOT NULL COMMENT '記事の本文',
     `status`     VARCHAR(20)     NOT NULL COMMENT '記事のステータス',
-    `author_id`  BIGINT UNSIGNED NOT NULL COMMENT '記事作成者のユーザID',
+    -- `author_id`  BIGINT UNSIGNED NOT NULL COMMENT '記事作成者のユーザID',
     `created_at` DATETIME(6)     NOT NULL COMMENT 'レコードの作成日時',
-    `updated_at` DATETIME(6)     NOT NULL COMMENT 'レコードの更新日時',
+    -- `updated_at` DATETIME(6)     NOT NULL COMMENT 'レコードの更新日時',
     PRIMARY KEY (`id`)
 ) ENGINE=INNODB DEFAULT CHARSET=utf8mb4 COMMENT='ブログ記事';
